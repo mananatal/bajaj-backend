e0b614c27f6fb350192ec3d7abd816c61b8fd22f
mananatal <mananatal25@gmail.com>
2024-11-21 19:53:44 +0530

    Increase file size limit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin:process.env.CORS_ORIGIN,
     Credentials:true
 }));
-app.use(express.json({limit:"20kb"}));
+app.use(express.json({limit:"20mb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true, limit: "20kb"}));
